32f985eee46733d086c1c104a26da18944a27760
Vitalii57 <zubkov127@gmail.com>
2023-05-21 22:16:07 +0300

    test2

--- hoc_time/src/App.js
+++ hoc_time/src/App.js
@@ -1,59 +1,103 @@
 import React, { useState } from "react";
 import { nanoid } from "nanoid";
-import Hoc_time from "./Hoc_time";
+import WithTextMark from "./hoc/HocTime";
 
-function DateTime(props) {
-  return <p className="date">{props.date}</p>;
+export function New(props) {
+  return (
+    <div className="wrap-item wrap-item-new">
+      <span className="label">New!</span>
+      {props.children}
+    </div>
+  );
+}
+
+export function Popular(props) {
+  return (
+    <div className="wrap-item wrap-item-popular">
+      <span className="label">Popular!</span>
+      {props.children}
+    </div>
+  );
+}
+
+function Article(props) {
+  return (
+    <div className="item item-article">
+      <h3>
+        <a href="#">{props.title}</a>
+      </h3>
+      <p className="views">Прочтений: {props.views}</p>
+    </div>
+  );
 }
 
 function Video(props) {
   return (
-    <div className="video">
+    <div className="item item-video">
       <iframe
-        title={props.url}
         src={props.url}
         frameBorder="0"
         allow="autoplay; encrypted-media"
         allowFullScreen
       ></iframe>
-      <DateTime date={props.date} />
+      <p className="views">Просмотров: {props.views}</p>
     </div>
   );
 }
 
-function VideoList({ props }) {
-  return props.list.map((item) => (
-    <Video key={nanoid()} url={item.url} date={item.date} />
-  ));
+function List({ props }) {
+  return props.map((item) => {
+    switch (item.type) {
+      case "video":
+        return (
+          <WithTextMark Component={Video} key={nanoid()} props={{ ...item }} />
+        );
+
+      case "article":
+        return (
+          <WithTextMark
+            Component={Article}
+            key={nanoid()}
+            props={{ ...item }}
+          />
+        );
+    }
+  });
 }
 
 export default function App() {
   const [list, setList] = useState([
     {
+      type: "video",
       url: "https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0",
-      date: "2017-07-31 13:24:00",
+      views: 50,
     },
     {
+      type: "video",
       url: "https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0",
-      date: "2018-03-03 12:10:00",
+      views: 12,
     },
     {
-      url: "https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0",
-      date: "2018-02-03 23:16:00",
+      type: "article",
+      title: "Невероятные события в неизвестном поселке...",
+      views: 175,
     },
     {
-      url: "https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0",
-      date: "2018-01-03 12:10:00",
+      type: "article",
+      title: "Секретные данные были раскрыты!",
+      views: 1532,
     },
     {
+      type: "video",
       url: "https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0",
-      date: "2018-01-01 16:17:00",
+      views: 4253,
     },
     {
-      url: "https://www.youtube.com/embed/TxbE79-1OSI?rel=0&amp;controls=0&amp;showinfo=0",
-      date: "2017-12-02 05:24:00",
+      type: "article",
+      title: "Кот Бегемот обладает невероятной...",
+      views: 12,
     },
   ]);
 
-  return <Hoc_time Component={VideoList} props={{ list }} />;
+  return <List props={list} />;
 }
